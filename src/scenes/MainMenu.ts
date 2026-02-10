3e43e812d986f615ac0392021d1378aca4b99351
Arc1llusion <arc1llusion@gmail.com>
2026-02-10 15:18:57 -0500

    Title background

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,10 +1,10 @@
-import { Scene, GameObjects } from 'phaser';
+import { GameObjects } from 'phaser';
+import { Align } from '../util/align';
+import { BaseScene } from './BaseScene';
 
-export class MainMenu extends Scene
+export class MainMenu extends BaseScene
 {
     background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
 
     constructor ()
     {
@@ -13,15 +13,12 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(512, 384, 'background');
+        super.create();
 
-        this.logo = this.add.image(512, 300, 'logo');
-
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.cameras.main.setBackgroundColor(0x000000);
+        
+        this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        Align.scaleToGameWidth(this.background, 1, this);
 
         this.input.once('pointerdown', () => {
 
